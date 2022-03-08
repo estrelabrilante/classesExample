879f669f816a7d515ce66cd7504189e8818e2b86
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-03-08 18:45:57 -0500

    classes are passes by a reference

--- main.swift
+++ main.swift
@@ -8,11 +8,13 @@
 import Foundation
 //inilalise
 let skeleton = Enemy(health:100, attackhealth: 10)
-//other refernce to same object
-let skeleton1 = skeleton
+//other reference to same object
+//let skeleton1 = skeleton
+
+let skeleton1 = Enemy(health: 100, attackhealth: 10)
 //print : 100-10 = 90
 print(skeleton.takeDamage(amount: 10))
-//print 90 as classes are passed by reference
+//print 100
 print(skeleton1.health)
 skeleton.attack();
 skeleton.move();
