d1310d851fef82f7d2d98067f49037916fe51ae4
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-03-08 18:49:11 -0500

    inheritance in classes

--- main.swift
+++ main.swift
@@ -9,12 +9,17 @@ import Foundation
 //inilalise
 let skeleton = Enemy(health:100, attackhealth: 10)
 //other reference to same object
-//let skeleton1 = skeleton
+let skeleton1 = skeleton
 
-let skeleton1 = Enemy(health: 100, attackhealth: 10)
+//let skeleton1 = Enemy(health: 100, attackhealth: 10)
 //print : 100-10 = 90
-print(skeleton.takeDamage(amount: 10))
-//print 100
+//print(skeleton.takeDamage(amount: 10))
+skeleton1.takeDamage(amount: 10)
+skeleton1.takeDamage(amount: 10)
+skeleton.takeDamage(amount: 10)
+//print 70
+print(skeleton.health)
+//print 70
 print(skeleton1.health)
 skeleton.attack();
 skeleton.move();
